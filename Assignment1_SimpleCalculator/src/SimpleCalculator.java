3c752b9285e9b3668edbc4a5117f7b59fdaeb50d
npham4617 <nganpham1007@gmail.com>
2024-02-03 12:33:49 -0800

    Upload all files

--- Assignment1_SimpleCalculator/src/SimpleCalculator.java
+++ Assignment1_SimpleCalculator/src/SimpleCalculator.java
@@ -2,7 +2,21 @@ import java.util.Scanner;
 
 public class SimpleCalculator {
 	
-	static void myCalculator (int number1, int number2, int operation) {
+	public static boolean checkUserInput(String userIn) {
+		// Make sure the input value being a string
+		boolean flag = false;
+		int index = 0;
+		
+		while (index < userIn.length()) {
+			if (!(userIn.charAt(index) >= '0' && userIn.charAt(index) <='9')) {
+				flag = true;	
+			}
+			index = index + 1;
+		}
+		return flag;
+	}
+	
+	public static void myCalculator (int number1, int number2, int operation) {	
 		int result;
 	    if (operation == 1) {
 	    	result = number1 + number2;
@@ -18,25 +32,40 @@ public class SimpleCalculator {
 	    }
 	    if (operation == 4) {
 	    	if (number2 == 0) {
-	    		result = 0;
+	    		System.out.println("Cannot divide by zero");
 	    	}
 	    	else {
-	    		result = number1 / number2;
+	    		float resultDi = (float) number1 / number2;
+	    		System.out.println("Result: " + number1 + " / " + number2 + " = " + String.format("%.2f", resultDi));
 	    	}
-	    	System.out.println("Result: " + number1 + " / " + number2 + " = " + result);
 	    }
 	}
 	
 	public static void main (String[] arg) {
 		System.out.println("Welcome to the Simple Calculator!");
-		String action;
+		Scanner scanner = new Scanner(System.in);  		// Create a Scanner object
+		String action, userIn;
+
 		do {
-			Scanner userIn = new Scanner(System.in);  		// Create a Scanner object
 		    System.out.print("\nEnter the first number: ");
-	        int number1 = userIn.nextInt();					// Read user input
+	        userIn = scanner.nextLine();
 	        
+	        while (checkUserInput(userIn)) {
+	        	System.out.print("Invalid input. Re-enter the first number (nonletter): ");
+	        	userIn = scanner.nextLine();
+	        }
+		    
+	        int number1 = Integer.parseInt(userIn);					// Read user input
+
 		    System.out.print("Enter the second number: ");
-		    int number2 = userIn.nextInt();	 				// Read user input
+		    userIn = scanner.nextLine();
+		    
+		    while (checkUserInput(userIn)) {
+	        	System.out.print("Invalid input. Re-enter the second number (nonletter): ");
+	        	userIn = scanner.nextLine();
+	        }
+		    
+		    int number2 = Integer.parseInt(userIn);	 				// Read user input
 		    
 		    String myMultiLineText = """
 		    		\nSelect an operation:
@@ -47,16 +76,25 @@ public class SimpleCalculator {
 	                """;
 		    System.out.println(myMultiLineText);
 		    System.out.print("Enter the operation number: " );
-		    int operation = userIn.nextInt();	 			// Read user input
+		    userIn = scanner.nextLine();
+	        
+	        while (checkUserInput(userIn) || Integer.parseInt(userIn) < 1 || Integer.parseInt(userIn) > 4) {
+	        	System.out.print("Re-enter the operation number (1-4): ");
+	        	userIn = scanner.nextLine();
+	        }
+	        	    		    
+		    int operation = Integer.parseInt(userIn);	 			// Read user input
+		    
 		    myCalculator (number1, number2, operation);
 		   
-		    Scanner userOp = new Scanner(System.in); 
 		    System.out.print("\nDo you want to perform another calculation? (yes/no): ");
-		    action = userOp.nextLine(); 
+		    action = scanner.nextLine(); 
 		    
 		    if (action.equals("no"))
 		    	System.out.println("\nGoodbye!");
 		} while (action.equals("yes"));
+		scanner.close();
 	}
+	
 
 }
